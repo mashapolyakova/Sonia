fbd5c4b80a5f004866141679aa06c5678065c02f
Maria Polyakova <polyakovam1708@gmail.com>
2019-03-23 12:39:11 +0300

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -153,7 +153,7 @@ file_path = os.getcwd()
 path = file_path + '/decoder_tchaik'
 mid = run(number, path)
 mid.save("input.mid")
-path = file_path + '/FluidR3_GM.sf3'
+path = file_path + '/FluidR3_GM.sf2'
 fs = FluidSynth(path)
 path = input()
 fs.midi_to_audio('input.mid', path + '/output.wav')
